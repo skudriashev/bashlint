716ef57865d74d68a90168179ac55760c02469be
skudriashev <stas.kudriashev@gmail.com>
2020-03-21 10:22:27 -0700

    Drop support of Python 2.7

--- setup.py
+++ setup.py
@@ -28,8 +28,6 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
